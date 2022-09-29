0110e7b8a8f71512dcb8b017a76668f787d53da8
SevenSolaris <derekforgione@gmail.com>
2022-09-28 19:26:30 -0700

    Getting ever closer to completion.

--- src/task.rs
+++ src/task.rs
@@ -1,8 +1,14 @@
 #![allow(unused)]
 
-use std::time::Instant;
+use serde::*;
 
-use egui::{self, *};
+use std::{time::Instant, collections::HashMap};
+use chrono::prelude::*;
+
+use eframe::epaint::Shadow;
+use egui::{self, *, text_edit::TextEdit};
+
+use crate::{ui_extensions::UiExtensions, print_unimplemented_message, function_bar};
 
 /// TODO
 ///  Create clocked task. The clocked task will have a start/stop button which will measure the
@@ -49,19 +55,6 @@ fn next_index() -> usize {
 pub enum TaskData {
     Single(bool),
     Multiple(Vec<Task>),
-}
-
-#[derive(serde::Deserialize, serde::Serialize)]
-pub struct Task {
-    pub title: String,
-    pub description: Option<String>,
-    pub id: Id,
-    pub tag: usize,    
-    pub data: TaskData,
-    #[serde(skip)]
-    remove_me: bool,
-    #[serde(skip)]
-    editing: bool,
 }
 
 impl TaskData {
@@ -119,32 +112,39 @@ impl TaskData {
 
 }
 
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Task {
+    pub title: String,
+    pub description: Option<String>,
+    pub id: Id,
+    pub creation_time: chrono::DateTime<Utc>,
+    pub data: TaskData,
+    #[serde(skip)]
+    remove_me: bool,
+    #[serde(skip)]
+    editing: bool,
+}
+
 impl Task {
-    pub fn new<T: Into<String>>(title: T, id: Id) -> Self {
+    pub fn new<T: Into<String>>(title: T) -> Self {
+        let time = Utc::now();
         Self {
             title: title.into(),
             description: None,
-            id,
-            tag: next_index(),
+            id: Id::new(time),
+            creation_time: time,
             data: TaskData::single(),
             remove_me: false,
             editing: false,
         }
     }
 
-    pub fn group<T: Into<String>>(title: T, id: Id) -> Self {
-        Self {
-            title: title.into(),
-            description: None,
-            id,
-            tag: next_index(),
-            data: TaskData::multi(),
-            remove_me: false,
-            editing: false,
-        }
+    pub fn group<T: Into<String>>(title: T) -> Self {
+        Self::new(title)
+            .with(TaskList::new())
     }
 
-    pub fn with(mut self, tasks: Vec<Task>) -> Self {
+    pub fn with(mut self, tasks: TaskList) -> Self {
         self.data = TaskData::Multiple(tasks);
         self
     }
@@ -261,98 +261,98 @@ impl Task {
     }
 
     pub fn render_header(&mut self, ui: &mut Ui) -> Response {
-        ui.horizontal(|ui| {
-            let mark = ui.horizontal(|ui| {
-                let mut check = self.complete();
-                if self.editing {
-                    ui.set_visible(false);
-                }
-                let mark = ui.checkbox(&mut check, "");
-                if mark.changed() {
-                    self.toggle_complete();
-                }
-                mark
-            }).inner;
-            let title = if self.editing {
-                let resp = ui.text_edit_singleline(&mut self.title);
-                if resp.lost_focus() {
-                    self.editing = false;
-                }
-                resp
-            } else {
-                let resp = ui.label(self.title.as_str()).interact(Sense::click());
-                if resp.clicked() {
-                    self.editing = true;
-                }
-                resp
-            };
-            let right = ui.with_layout(Layout::right_to_left(Align::Min), |ui| {
-                let exit = ui.small_button(RichText::new("⊗").color(Color32::RED));
-                if exit.clicked() {
-                    self.remove_me = true;
-                }
-                let edit = ui.small_button(if self.editing { "✏" } else { "💾" });
-                if edit.clicked() {
-                    self.editing = !self.editing;
-                }
-                exit | edit
-            }).inner;
-            mark | title | right
-        }).response
+        Frame::group(ui.style())
+            .rounding(Rounding::none())
+            .show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    let mark = /* ui.horizontal (|ui| */ {
+                        let mut check = self.complete();
+                        if self.editing {
+                            ui.set_visible(false);
+                        }
+                        let mark = ui.ballot(&mut check);
+                        if mark.changed() {
+                            self.toggle_complete();
+                        }
+                        mark
+                    }/*).inner*/;
+                    let add = ui.button("⊞");
+                    if add.clicked() {
+                        self.add(Task::new("Untitled Task"));
+                    }
+                    let title = if self.editing {
+                        let resp = ui.text_edit_singleline(&mut self.title);
+                        if resp.lost_focus() {
+                            self.editing = false;
+                        }
+                        resp
+                    } else {
+                        let resp = ui.label(self.title.as_str()).interact(Sense::click());
+                        if resp.clicked() {
+                            self.editing = true;
+                        }
+                        resp
+                    };
+                    let right = ui.with_layout(Layout::right_to_left(Align::Min), |ui| {
+                        ui.spacing_mut().item_spacing.x = 4.0;
+                        let exit = ui.button(RichText::new("⊗").color(Color32::RED));
+                        if exit.clicked() {
+                            self.remove_me = true;
+                        }
+                        let edit = ui.button(if self.editing { "💾" } else { "✏" });
+                        if edit.clicked() {
+                            self.editing = !self.editing;
+                        }
+                        exit | edit
+                    }).inner;
+                    mark | title | right
+                }).response
+            }).inner
     }
 
     pub fn render(&mut self, ui: &mut Ui) -> Response {
         // This task will either be a single or a multi
         // TODO: Capture all Responses from ui to return to caller.
-        let id = ui.make_persistent_id(self.id);
-        match self.single() {
-            true => {
-                ui.push_id(id, |ui| self.render_header(ui)).inner
-            }
-            false => {
-                collapsing_header::CollapsingState::load_with_default_open(ui.ctx(), id, false)
-                    .show_header(ui, |ui| ui.horizontal(|ui| {
-                        self.render_header(ui)
-                    }))
-                    .body(|ui| {
-                        let tasks = self.data.tasks();
-                        render_list(ui, tasks);
-                    }).0
-            }
+        let id = ui.make_persistent_id(self.creation_time);
+        if self.single() {
+            ui.push_id(id, |ui| self.render_header(ui)).inner
+        } else {
+            collapsing_header::CollapsingState::load_with_default_open(ui.ctx(), id, true)
+                .show_header(ui, |ui| ui.horizontal(|ui| {
+                    self.render_header(ui)
+                }))
+                .body(|ui| {
+                    let tasks = self.data.tasks();
+                    render_list(ui, tasks);
+                }).0
         }
         
     }
     
 }
 
-// pub fn render_task(ui: &mut Ui, task: &mut Task) -> Response {
-//     ui.group(|group| {
-//         // Render Header
-//         if task.single() {
-            
-//         } else { // Multi   
-//             ui.indent(task.id.with("sub_tasks"), |sub| {
-                
-//             });
-//         }
-//     }).inner
-// }
-
 pub fn render_list(ui: &mut Ui, tasks: &mut Vec<Task>) -> Response {
     ui.vertical(|ui| {
 
         if !tasks.is_empty() {
             // Order by incomplete tasks first.
-            tasks.iter_mut()
-                .filter(|task| task.incomplete() )
-                .for_each(|task: &mut Task| {
-                    task.render(ui);
-                });
-            tasks.iter_mut()
-                .filter(|task| !task.incomplete() )
-                .for_each(|task: &mut Task| {
-                    task.render(ui);
-                });
+            let complete: Vec<usize> = tasks.iter()
+                .enumerate()
+                .filter(|(_, task)| task.incomplete() )
+                .map(|(i, _)| i)
+                .collect();
+            let incomplete: Vec<usize> = tasks.iter()
+                .enumerate()
+                .filter(|(_, task)| task.complete() )
+                .map(|(i, _)| i)
+                .collect();
+            let closure = |task: &mut Task| { task.render(ui); };
+            complete
+                .into_iter()
+                .for_each(|i| { tasks[i].render(ui); });
+            incomplete
+                .into_iter()
+                .for_each(|i| { tasks[i].render(ui); });
             // Check for removal
             let mut remove_at: Option<usize> = None;
             for (i, _) in tasks.iter().enumerate().filter(|(_, task)| task.remove_me ) {
@@ -363,11 +363,196 @@ pub fn render_list(ui: &mut Ui, tasks: &mut Vec<Task>) -> Response {
                 tasks.remove(index);
             }
         }
-
     }).response
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct TaskTree {
+    name: String,
+    unique_id: Id,
+    creation_time: DateTime<Utc>,
+    modified_time: DateTime<Utc>,
     tasks: TaskList,
-    
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TaskApp {
+    trees: Vec<TaskTree>,
+    #[serde(skip)]
+    new_task_popup: NewTaskPopupButton,
+}
+
+impl Default for NewTaskPopupButton {
+    fn default() -> Self {
+        Self {
+            title: "".to_owned(),
+            description: None,
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct NewTaskPopupButton {
+    // title, description
+    pub title: String,
+    pub description: Option<String>,
+}
+
+impl NewTaskPopupButton {
+
+    const NAME: &'static str = "new_task--popup";
+
+    pub fn show(&mut self, ui: &mut Ui, text: impl Into<WidgetText>, list: &mut TaskList) -> Response {
+
+        ui.menu_button(text.into(), |ui| {
+
+            // Set up style
+            ui.style_mut().visuals.window_rounding = Rounding::none();
+            ui.style_mut().visuals.popup_shadow = Shadow::default();
+            ui.style_mut().visuals.widgets.active.rounding = Rounding::none();
+            ui.style_mut().visuals.widgets.inactive.rounding = Rounding::none();
+            ui.style_mut().visuals.widgets.hovered.rounding = Rounding::none();
+            ui.style_mut().visuals.widgets.open.rounding = Rounding::none();
+            ui.style_mut().visuals.widgets.noninteractive.rounding = Rounding::none();
+
+
+            /// empty means that the description is None.
+            // underline format
+            let ul_fmt = TextFormat { underline: Stroke::new(1.0, Color32::WHITE), ..Default::default() };
+            // default format
+            let def_fmt = TextFormat::default();
+            // Add button text
+            let mut add_button_text = text::LayoutJob::default();
+            add_button_text.append("A", 0.0, ul_fmt.clone());
+            add_button_text.append("dd", 0.0, TextFormat::default());
+            let mut clear_button_text = text::LayoutJob::default();
+            clear_button_text.append("C", 0.0, ul_fmt.clone());
+            clear_button_text.append("lear", 0.0, TextFormat::default());
+            // Add/Remove Description button text
+            let mut description_button_text = text::LayoutJob::default();
+            if self.description.is_none() {
+                description_button_text.append("Add ", 0.0, TextFormat::default());
+                description_button_text.append("D", 0.0, ul_fmt.clone());
+                description_button_text.append("escription ", 0.0, TextFormat::default());
+            } else {
+                description_button_text.append("Remove ", 0.0, TextFormat::default());
+                description_button_text.append("D", 0.0, ul_fmt.clone());
+                description_button_text.append("escription ", 0.0, TextFormat::default());
+            }
+            // group frame and vertical layout
+            ui.group(|ui| { ui.vertical(|ui| {
+                // Check for Alt+A
+                if !self.title.is_empty()
+                && ui.ctx().input_mut().consume_key(Modifiers::ALT, Key::A) {
+                    let mut task = Task::new(self.title.clone());
+                    self.title.replace("");
+                    task.description = self.description.take();
+                    list.push(task);
+                }
+                // Check for Alt+C
+                if ui.ctx().input_mut().consume_key(Modifiers::ALT, Key::C) {
+                    self.title.replace("");
+                    self.description = None;
+                    ui.memory().request_focus(Id::new(Self::NAME).with(Id::new("title_text")));
+                }
+                // Check for Alt+D
+                if ui.ctx().input_mut().consume_key(Modifiers::ALT, Key::D) {
+                    if self.description.is_none() {
+                        self.description = Some(String::default());
+                        ui.memory().request_focus(Id::new(Self::NAME).with(Id::new("description_text")));
+                    } else {
+                        self.description = None;
+                        ui.memory().request_focus(Id::new(Self::NAME).with(Id::new("title_text")));
+                    }
+                }
+                // let title_focused = ui
+                //     .ctx()
+                //     .data()
+                //     .get_persisted(Id::new(Self::NAME))
+                //     .unwrap_or(false);
+                if !self.title.is_empty()
+                && ui.ctx().input_mut().consume_key(Modifiers::CTRL, Key::Enter) {
+                    let mut task = Task::new(self.title.clone());
+                    self.title.replace("");
+                    task.description = self.description.take();
+                    list.push(task);
+                }
+                ui.allocate_ui(vec2(300.0, ui.spacing().interact_size.y), |ui| {
+                    let title_text = ui.put(
+                        ui.max_rect(),
+                        TextEdit::singleline(&mut self.title)
+                            .hint_text("Title")
+                            .id(Id::new(Self::NAME).with(Id::new("title_text")))
+                            .font(FontSelection::Style(TextStyle::Monospace))
+                    );
+                    if title_text.lost_focus()
+                    && !self.title.is_empty()
+                    && ui.ctx().input().key_pressed(Key::Enter) {
+                        let mut task = Task::new(self.title.clone());
+                        self.title.replace("");
+                        task.description = self.description.take();
+                        list.push(task);
+                        ui.memory().request_focus(Id::new(Self::NAME).with(Id::new("title_text")));
+                    }
+                });
+                ui.allocate_ui(vec2(ui.available_width(), ui.spacing().interact_size.y * 4.0), |ui| {
+                    let description_text = self.description.as_mut().and_then(|description| {
+                        Some(ui.put(
+                            ui.max_rect(),
+                            TextEdit::multiline(description)
+                                .hint_text("Description")
+                                .desired_rows(4)
+                                .id(Id::new(Self::NAME).with(Id::new("description_text")))
+                                .font(FontSelection::Style(TextStyle::Monospace))
+                                .lock_focus(true)
+                        ))
+                    });
+                    if let Some(description_edit) = description_text {
+                        if description_edit.has_focus() && ui.ctx().input_mut().consume_key(Modifiers::CTRL, Key::Enter) {
+                            // This should cause a task to be added.
+                            print_unimplemented_message!("Ctrl+Enter in Description Text Edit.");
+                        }
+                    }
+                });
+                ui.allocate_ui(vec2(ui.available_width(), ui.spacing().interact_size.y), |ui| {
+                    if !self.title.is_empty() {
+                        ui.columns(2, |columns| {
+                            function_bar!(
+                                columns[0];
+                                add_button_text => {
+                                    let mut task = Task::new(self.title.clone());
+                                    self.title.replace("");
+                                    task.description = self.description.take();
+                                    list.push(task);
+                                }
+                                clear_button_text => {
+                                    self.title.replace("");
+                                    self.description = None;
+                                }
+                            );
+                            let description_btn = columns[1].put(columns[1].max_rect(), Button::new(description_button_text));
+                            if description_btn.clicked()
+                            || columns[1].ctx().input_mut().consume_key(Modifiers::CTRL, Key::D) {
+                                if self.description.is_some() {
+                                    self.description = None;
+                                } else {
+                                    self.description = Some(String::default());
+                                }
+                            }
+                        });
+                    } else {
+                        let description_btn = ui.put(ui.max_rect(), Button::new(description_button_text));
+                        if description_btn.clicked()
+                        || ui.ctx().input_mut().consume_key(Modifiers::CTRL, Key::D) {
+                            if self.description.is_some() {
+                                self.description = None;
+                            } else {
+                                self.description = Some(String::default());
+                            }
+                        }
+                    }
+                });
+            });});
+        }).response
+    }
 }
